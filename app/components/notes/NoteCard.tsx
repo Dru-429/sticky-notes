7e97a1a262964209df21e9d1fd50119173ef583b
Dru-429 <ect.dhruv@gmail.com>
2025-03-09 01:49:31 +0530

    Added notesdcard and fakedata

---
+++ app/components/notes/NoteCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface Note {
+    $id: number;
+    body: string;
+    colors: string; // Assuming colors is also a JSON string
+    position: string; // Assuming position is also a JSON string
+}
+
+const NoteCard: React.FC<{ note: Note }> = ({ note }) => {
+    try {
+
+        let position = JSON.parse(note.position);
+        const colors = JSON.parse(note.colors);
+        const body = JSON.parse(note.body);
+
+        return (
+            <div
+                className="card"
+                style={{
+                    backgroundColor: colors.colorBody,
+                }}
+            >
+                {body}
+            </div>
+        );
+        
+    }
+    catch (error) {
+        console.error('Error parsing note body:', error);
+        return <div>Error loading note</div>;
+    }
+};
+
+export default NoteCard;
--- app/components/notes/Notespage.tsx
+++ app/components/notes/Notespage.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import NoteCard from './NoteCard'
+import { fakeData as notes } from '@/app/assets/fakeData'
 
 const Notespage = () => {
-  return (
-    <div>Notespage</div>
-  )
+    return (
+        <div>
+            {notes.map((note) => (
+                <NoteCard note={note} key={note.$id} />
+            ))}
+        </div>
+    )
 }
 
 export default Notespage

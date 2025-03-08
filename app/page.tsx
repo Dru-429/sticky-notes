13323bbba66f5054ea899ba898b046f224ca7f48
Dru-429 <ect.dhruv@gmail.com>
2025-03-09 01:19:10 +0530

    Setup files

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import Notespage from './components/notes/Notespage'
+
+const page = () => {
+  return (
+    <div className=''>
+      <Notespage />
+    </div>
+  )
+}
+
+export default page

5a1e0412de6e4fa9521acdbc3d7d104e2abb7ae7
Dru-429 <ect.dhruv@gmail.com>
2025-03-11 10:51:24 +0530

    Sending req

--- app/components/notes/Notespage.tsx
+++ app/components/notes/Notespage.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import NoteCard from './NoteCard'
-import { fakeData as notes } from '@/app/assets/fakeData'
+import { databases } from '@/appwrite/config'
+// import { fakeData as notes } from '@/app/assets/fakeData'
 
 const Notespage = () => {
+
+    const [notes, setNotes] = useState([])
+
+    const init = async () => {
+        const response = await databases.listDocuments(
+            process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+            process.env. NEXT_PUBLIC_APPWRITE_COLLECTION_NOTES_ID   
+        )
+
+        console.log(response)
+    }
+
+    useEffect( () => {
+        init()
+    }, [])
+
     return (
         <div>
             {notes.map((note) => (

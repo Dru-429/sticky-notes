ecaf8e7dee972bd7c216607c4ba4aad65750dbd3
Dru-429 <ect.dhruv@gmail.com>
2025-04-14 07:27:20 +0530

    Added Notes page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
 import React from 'react'
-import Notespage from './components/notes/Notespage'
 import NoteProvider from '@/context/NoteContext'
 import { Boxes } from '@/components/ui/background-boxes'
+import Notespage from '../components/notes/Notespage'
 
 const page = () => {
   return (
-    <div className='w-screen h-screen'>
-      <LandingPage />
+    <div className='w-screen h-screen' id='app'>
+      <NoteProvider>
+        <Boxes />
+        <Notespage />
+      </NoteProvider>
     </div>
   )
 }
